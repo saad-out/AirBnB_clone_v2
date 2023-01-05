47752e13a32f62f3e43ee779c08d3b7090235da3
Dannyfsp <femoscopilala@gmail.com>
2023-01-04 17:37:40 -0800

    adding MySQL to storage engine and updating BaseModel

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""
+This module instantiates an object of class FileStorage/DBStorage
+"""
+import os
+from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
 
 
-storage = FileStorage()
+storage = DBStorage() if os.getenv(
+    'HBNB_TYPE_STORAGE') == 'db' else FileStorage()
+"""A unique DBStorage/FileStorage instance for all models"""
 storage.reload()
