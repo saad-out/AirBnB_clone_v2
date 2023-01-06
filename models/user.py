f419db26a382fbd9d1cc60eb1308373fa4096eca
Dannyfsp <femoscopilala@gmail.com>
2023-01-05 19:22:39 -0800

    fix test cases for user and place class

--- models/user.py
+++ models/user.py
@@ -10,24 +10,24 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
     email = Column(
-            String(128), nullable=False
+        String(128), nullable=False
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     password = Column(
-            String(128), nullable=False
+        String(128), nullable=False
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     first_name = Column(
-            String(128)
+        String(128)
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     last_name = Column(
-            String(128)
+        String(128)
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     places = relationship(
-            'Place',
-            cascade='all, delete, delete-orphan',
-            back_populates='user'
+        'Place',
+        cascade='all, delete, delete-orphan',
+        back_populates='user'
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
     reviews = relationship(
-            'Review',
-            cascade='all, delete, delete-orphan',
-            back_populates='user'
+        'Review',
+        cascade='all, delete, delete-orphan',
+        back_populates='user'
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
