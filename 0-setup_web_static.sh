8d5a087f5cc1348bcebad1ef142f0d4829016736
saad-out <outsaad03@gmail.com>
2023-01-21 10:42:37 +0000

    use 'service' & append after 'server_name*'

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,5 +6,5 @@ mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 echo "Hello from web_static" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '/root \/var\/www\/html;/a \\tlocation /web_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-nginx -s reload
+sed -i '/^\tserver_name.*/a \\tlocation /web_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart
