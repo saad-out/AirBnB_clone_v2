5b68403f600364bbe9a0bfe8f0368f0d0b3a0104
saad-out <outsaad03@gmail.com>
2023-02-05 15:09:59 +0000

    List states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Flask application to list States
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session(request):
+    """
+    Close session after each request
+    """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states():
+    """
+    List all states
+    """
+    states = [state for state in storage.all(State).values()]
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
